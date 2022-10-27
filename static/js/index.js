23e54366a28baae709cf72afe5297046ed3585ea
Nandan Bilagi <87522041+nandanpytech@users.noreply.github.com>
2022-10-27 22:10:03 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -26,7 +26,6 @@ let url_=fetch("https://restrajesh.herokuapp.com",{
     "Content-type":"application/json; charset=UTF-8",
     "Access-Control-Allow-Origin": "*",
   },
-  body: JSON.stringify(data),
 })
 .then(res=>res.json())
 .then(data=>{
